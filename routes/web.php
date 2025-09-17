c14a1cd2e5627d856521bc3b57f4b6648239f3c3
Primarizki Ahmad <primarizkiahmad.h@gmail.com>
2025-09-17 23:57:15 +0700

    new routes config

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LaporanController;
+use App\Http\Controllers\PengaduanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+
+Route::get('/pengaduan/create', [PengaduanController::class, 'create'])->name('pengaduan.create');
+Route::post('/pengaduan', [PengaduanController::class, 'store'])->name('pengaduan.store');
+Route::get('/pengaduan/{id}', [PengaduanController::class, 'show'])->name('pengaduan.show');
+Route::post('/pengaduan/{id}/tindak-lanjut', [PengaduanController::class, 'tindakLanjut'])->name('pengaduan.tindakLanjut');
+
+// Route::get('/form', [LaporanController::class, 'showForm'])->name('form')->middleware('auth');
+// Route::post('/form', [LaporanController::class, 'store'])->name('form.store')->middleware('auth');
+
+Route::get('/login', [UserController::class, 'showLogin'])->name('login');
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login.submit');
+Route::post('/logout', [UserController::class, 'logout'])->name('logout');
+
