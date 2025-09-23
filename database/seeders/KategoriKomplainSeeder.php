65c25a7ef091975674e233a106e72a995daede26
Primarizki Ahmad <primarizkiahmad.h@gmail.com>
2025-09-23 13:26:50 +0700

    fixing kategori

--- database/seeders/KategoriKomplainSeeder.php
+++ database/seeders/KategoriKomplainSeeder.php
@@ -13,7 +13,7 @@ class KategoriKomplainSeeder extends Seeder
             ['jenis_komplain' => 'Akademik', 'deskripsi_komplain' => 'Masalah perkuliahan, kurikulum, nilai, dll.'],
             ['jenis_komplain' => 'Fasilitas', 'deskripsi_komplain' => 'Sarana & prasarana kampus'],
             ['jenis_komplain' => 'Kekerasan', 'deskripsi_komplain' => 'Bullying, pelecehan, atau kekerasan lainnya'],
-            ['jenis_komplain' => 'Dosen', 'deskripsi_komplain' => 'Sikap atau pelayanan dosen'],
+            ['jenis_komplain' => 'Kemahasiswaan', 'deskripsi_komplain' => 'Sikap atau pelayanan dari pihak kemahasiswaan'],
             ['jenis_komplain' => 'Lainnya', 'deskripsi_komplain' => 'Komplain di luar kategori utama'],
         ];
 
