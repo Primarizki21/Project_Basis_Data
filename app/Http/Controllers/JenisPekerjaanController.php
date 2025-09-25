e46b1812164277108771a6fdcdc0b4253beffc2a
Primarizki21 <primarizkiahmad.h@gmail.com>
2025-09-25 23:19:42 +0700

    new controller but still disable

---
+++ app/Http/Controllers/JenisPekerjaanController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JenisPekerjaan; // <-- Import modelnya
+use Illuminate\Http\Request;
+
+class JenisPekerjaanController extends Controller
+{
+    // Sementara matikan dulu
+    // public function index()
+    // {
+    //     $pekerjaan = JenisPekerjaan::all();
+    //     return response()->json($pekerjaan);
+    // }
+}
