a760949467110d9a0ccf8c75fce145f69ed2fa54
Primarizki21 <primarizkiahmad.h@gmail.com>
2025-09-25 23:19:09 +0700

    new jenis pekerjaan model and modifying pekerjaan column

---
+++ app/Models/JenisPekerjaan.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisPekerjaan extends Model
+{
+    use HasFactory;
+    protected $table = 'jenis_pekerjaan';
+    protected $primaryKey = 'jenis_pekerjaan_id';
+    protected $keyType = 'int';
+    public $incrementing = true;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'nama_pekerjaan',
+    ];
+
+    public function pekerjaanpk()
+    {
+        return $this->hasOne(User::class, 'jenis_pekerjaan_id');
+    }
+}
